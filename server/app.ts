2671b5ffa775b70448fb435e5544310b6b304afa
Hamid <hamidali006789@gmail.com>
2024-03-20 09:34:35 +0500

    course model, In C controller (upload course,  edit course,  get single course ---- without purchasing, get All courses ---- without purchasing and createCourse service

--- server/app.ts
+++ server/app.ts
@@ -6,14 +6,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddlware } from "./middleware/error";
 import userRouter from "./routes/user.route";
-
-// import httpStatus from 'http-status';
-
-// import { ErrorHandler } from './utils/errorHandler';
-// import defaultVars from './config/defaultVars';
-// import { errorMiddlware } from './middleware/errorMiddware';
-// import router from './routes';
-// import swaggerRouter from './docs';
+import courseRouter from "./routes/course.route";
 
 // body parser
 app.use(express.json({ limit: "50mb" }));
@@ -26,6 +19,8 @@ app.use(cors({ origin: process.env.PORT }));
 
 // routes
 app.use("/api/v1", userRouter);
+
+app.use("/api/v1", courseRouter);
 
 // testing api
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
